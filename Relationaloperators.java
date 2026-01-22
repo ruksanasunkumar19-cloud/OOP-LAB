5ea539e6eb3e21f4a53c6146c29c8e3b4ca1b0b1
ruksanasunkumar19-cloud <ruksanasunkumar19@gmail.com>
2026-01-22 14:56:55 +0530

    Add files via upload

---
+++ Relationaloperators.java
@@ -0,0 +1,12 @@
+public class Relationaloperators{
+public static void main(String[] args){
+int A=5;
+int B=2;
+System.out.println(A==B);
+System.out.println(A !=B);
+System.out.println(A > B);
+System.out.println(A < B);
+System.out.println(A >= B);
+System.out.println(A <= B);
+}
+}
