f92994556412cace82ac6a8f18123f12402902d1
ruksanasunkumar19-cloud <ruksanasunkumar19@gmail.com>
2026-01-22 15:07:02 +0530

    Add files via upload

---
+++ Logicaloperators.java
@@ -0,0 +1,8 @@
+public class Logicaloperators{
+public static void main(String[] args){
+int age =52;
+System.out.println(age >18 && age <60);
+System.out.println(age <18 || age <60);
+System.out.println(!(age >18));
+}
+}
