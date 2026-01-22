90b43a6f9a14c0c7ef89fe3f3859969e27c10570
ruksanasunkumar19-cloud <ruksanasunkumar19@gmail.com>
2026-01-22 14:50:20 +0530

    Add files via upload

---
+++ Arithmeticoperators.java
@@ -0,0 +1,11 @@
+public class Arithmeticoperators{
+public static void main(String[] args){
+int A=5;
+int B=2;
+System.out.println("ADDITION:"+(A+B));
+System.out.println("SUBTRACTION:"+(A-B));
+System.out.println("MULTIPLICATION:"+(A*B));
+System.out.println("DIVISION:"+(A/B));
+System.out.println("MODULUS:"+(A%B));
+}
+}
