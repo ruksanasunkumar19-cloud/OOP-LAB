9081dc812549689fadab9883c24dc403552cda87
ruksanasunkumar19-cloud <ruksanasunkumar19@gmail.com>
2026-01-22 15:14:37 +0530

    Add files via upload

---
+++ Assignmentoperators.java
@@ -0,0 +1,13 @@
+public class Assignmentoperators{
+public static void main(String[] args){
+int a=5;
+a +=5;
+System.out.println(a);
+a -=4;
+System.out.println(a);
+a *=2;
+System.out.println(a);
+a /=5;
+System.out.println(a);
+}
+}
