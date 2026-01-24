b7868d78cd2dcd3b19c0823ca395776f27d7c217
ruksanasunkumar19-cloud <ruksanasunkumar19@gmail.com>
2026-01-24 10:25:43 +0530

    Add files via upload

---
+++ Alldatatypes.java
@@ -0,0 +1,13 @@
+class Alldatatypes {
+    public static void main(String[] args) {
+        int age = 52;
+        double salary = 30000.50;
+        String name = "joeee";
+        boolean isStudent = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Name: " + name);
+        System.out.println("Student: " + isStudent);
+    }
+}
