18655a18cbef5cdf106310193abaad019e6acec1
ruksanasunkumar19-cloud <ruksanasunkumar19@gmail.com>
2026-03-31 15:24:58 +0530

    Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,26 +1,25 @@
 class Calculator {
 
-    // Public method - add two integers
+   
     public int add(int x, int y) {
         return x + y;
     }
 
-    // Protected method - add three integers
+   
     protected int add(int x, int y, int z) {
         return x + y + z;
     }
 
-    // Private method - add two decimal numbers
+    
     private double add(double x, double y) {
         return x + y;
     }
 
-    // Public method to access private method
     public void printDecimalAddition(double x, double y) {
         System.out.println("Decimal Sum: " + add(x, y));
     }
 
-    // Main method to test
+
     public static void main(String[] args) {
         Calculator calc = new Calculator();
 
